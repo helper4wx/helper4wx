6d36e5fc9c4caa68576965c749560cd8f65b86d7
helper4wx <helper4wx@gmail.com>
2013-10-22 10:52:03 +0800

    3rd commit

--- src/wx.js
+++ src/wx.js
@@ -4,15 +4,16 @@ wx_helper = {};
 $("#wx_helper_box").remove();
 
 (function(H, $) {
-	H.boxHTML = '<div><button id="wxh_show" style="float:left">群发助手</button></div><div style="clear:both"></div><div id="wxh_main" style="background-color:#E9E9E9;padding:5px;display:none;border:2px dotted #6B747A;"><div style="float:left;"><span id="wxh_friends_tab" style="cursor:pointer;background-color:#999;color:#fff">选择联系人</span>&nbsp;&nbsp;&nbsp;&nbsp;<span id="wxh_groups_tab" style="cursor:pointer">选择群组</span></div><div style="clear:both"></div><div style="height:300px;float:left;width:300px;border:1px solid #999;overflow:auto"><div id="wxh_friends_list"></div><div id="wxh_groups_list" style="display:none"></div></div><div style="clear:both"></div><div style="margin:5px;"><select style="width:290px;height:22px;margin:5px" id="wxh_templates"><option>选择群发模板</option></select><div style="clear:both"></div><textarea id="wxh_text" style="float:left;height:80px;width:240px;"></textarea><button id="wxh_send" style="margin-top:40px">群发</button></div><div style="clear:both"></div></div>';
+	H.boxHTML = '<div><button id="wxh_show" style="float:left">群发助手</button><button id="wxh_hide" style="display:none;float:right">关闭</button></div><div style="clear:both"></div><div id="wxh_main" style="background-color:#E9E9E9;padding:5px;display:none;border:2px dotted #6B747A;"><div style="float:left;"><span id="wxh_friends_tab" style="cursor:pointer;background-color:#999;color:#fff">选择联系人</span>&nbsp;&nbsp;&nbsp;&nbsp;<span id="wxh_groups_tab" style="cursor:pointer">选择群组</span></div><div style="clear:both"></div><div style="height:300px;float:left;width:300px;border:1px solid #999;overflow:auto"><div id="wxh_friends_list"></div><div id="wxh_groups_list" style="display:none"></div></div><div style="clear:both"></div><div style="margin:5px;"><select style="width:290px;height:22px;margin:5px" id="wxh_templates"><option>选择群发模板</option></select><div style="clear:both"></div><textarea id="wxh_text" style="float:left;height:80px;width:240px;"></textarea><button id="wxh_send" style="margin-top:40px">群发</button></div><div style="clear:both"></div></div>';
 	H.templates = ["恭喜发财,大吉大利!", "哈喽啊,{{名字}}! 介绍个好东西给你~", "我在这儿,欢迎来往,O(∩_∩)O哈哈~"];
 	H.addrList = {
-		groups : [],
-		publics : [],
-		friends : []
+		groups: [],
+		publics: [],
+		friends: []
 	};
 	H.taskList = [];
 	H.ready = false;
+	H.dataReady = false;
 	H.openChat = function(username) {
 		var def = new $.Deferred();
 		var a = $("#con_item_" + username);
@@ -112,12 +113,12 @@ $("#wx_helper_box").remove();
 		var dv = document.createElement("div");
 		dv.id = "wx_helper_box"
 		$(dv).css({
-			width : "320px",
-			position : "absolute",
-			top : "10px",
-			left : "0px",
-			zIndex : 100001,
-			fontSize : "14px"
+			width: "320px",
+			position: "absolute",
+			top: "10px",
+			left: "0px",
+			zIndex: 100001,
+			fontSize: "14px"
 		});
 
 		$(dv).html(H.boxHTML);
@@ -137,7 +138,7 @@ $("#wx_helper_box").remove();
 		for (var i = 0; i < H.addrList.friends.length; i++) {
 			f += buildTr(H.addrList.friends[i])
 		}
-		for ( i = 0; i < H.addrList.groups.length; i++) {
+		for (i = 0; i < H.addrList.groups.length; i++) {
 			g += buildTr(H.addrList.groups[i])
 		}
 		if (f) {
@@ -148,6 +149,7 @@ $("#wx_helper_box").remove();
 			g = '<table style="font-size:12px;text-align:left"><thead><tr><th><input id="wxh_groups_chkall" type="checkbox"/></th><th style="color:black">全选所有群组</th></tr></thead><tbody>' + g + '</tbody></table>';
 			$gp.html(g);
 		}
+
 		function buildTr(addr) {
 			var s = '<tr><td>';
 			s += '<input type="checkbox" username="' + addr.username + '" nickname="' + addr.nickname + '"></td>'
@@ -156,7 +158,7 @@ $("#wx_helper_box").remove();
 		}
 
 		var t = ""
-		for ( i = 0; i < H.templates.length; i++) {
+		for (i = 0; i < H.templates.length; i++) {
 			t += '<option>' + H.templates[i] + '</option>';
 		}
 		$("#wxh_templates").append(t);
@@ -171,34 +173,48 @@ $("#wx_helper_box").remove();
 		var def = new $.Deferred();
 		var box = $("#wx_helper_box");
 		box.on("click", "#wxh_show", function() {
+			if (!H.dataReady) {
+				H.loadAddr().done(function() {
+					H.dataReady = true;
+					H.syncBox();
+				});
+			}
 			$("#wxh_main").toggle(200);
+			$("#mask").toggle();
+			$("#wxh_hide").toggle();
+		});
+		box.on("click", "#wxh_hide", function() {
+			$("#wxh_main").hide(200);
+			$("#mask").hide();
+			$("#wxh_hide").hide();
 		});
 		box.on("click", "#wxh_friends_tab", function() {
 			$("#wxh_groups_list").hide();
 			$("#wxh_friends_list").show();
 			$("#wxh_friends_tab").css({
-				backgroundColor : "#999",
-				color : "#fff"
+				backgroundColor: "#999",
+				color: "#fff"
 			});
 			$("#wxh_groups_tab").css({
-				backgroundColor : "#fff",
-				color : "#999"
+				backgroundColor: "#fff",
+				color: "#999"
 			});
 		});
 		box.on("click", "#wxh_groups_tab", function() {
 			$("#wxh_friends_list").hide();
 			$("#wxh_groups_list").show();
 			$("#wxh_friends_tab").css({
-				backgroundColor : "#fff",
-				color : "#999"
+				backgroundColor: "#fff",
+				color: "#999"
 			});
 			$("#wxh_groups_tab").css({
-				backgroundColor : "#999",
-				color : "#fff"
+				backgroundColor: "#999",
+				color: "#fff"
 			});
 		});
 		box.on("click", "#wxh_friends_chkall", chkAll);
 		box.on("click", "#wxh_groups_chkall", chkAll);
+
 		function chkAll(evt) {
 			var ipt = evt.target;
 			var $tbd = $(ipt).parent().parent().parent().next();
@@ -226,18 +242,6 @@ $("#wx_helper_box").remove();
 		return def.promise();
 	};
 
-	H.boot = function() {
-		H.loadAddr().done(function() {
-			H.addBox().done(function() {
-				H.syncBox().done(function() {
-					H.bindBox().done(function() {
-						H.ready = true;
-					});
-				});
-			});
-		});
-	};
-
 	H.multiSend = function() {
 		if (!H.ready) {
 			return;
@@ -249,7 +253,7 @@ $("#wx_helper_box").remove();
 		H.taskList = [];
 		var str = $("#wxh_text").val().trim();
 		var hasName = false;
-		if(str.indexOf("{{名字}}")>-1){
+		if (str.indexOf("{{名字}}") > -1) {
 			hasName = true;
 		}
 		var addrs = $("#wx_helper_box tbody input[type=checkbox]");
@@ -270,12 +274,15 @@ $("#wx_helper_box").remove();
 			return;
 		} else {
 			var cfm = "发送给:" + nickList.join(", ") + ", 共" + nickList.length + "个好友或群组,确定么?";
-			if(!hasName){
+			if (!hasName) {
 				cfm += "(发送会耗时一段时间,不要操作网页:)";
-			}else{
+			} else {
 				cfm += '(注意:"{{名字}}"通配符将替换成你在通讯录中给Ta起的"备注名"噢!!)';
 			}
 			if (window.confirm(cfm)) {
+				$("#wxh_main").hide(200);
+				$("#mask").hide();
+				$("#wxh_hide").hide();
 				H.exec();
 			} else {
 				H.taskList = [];
@@ -293,7 +300,17 @@ $("#wx_helper_box").remove();
 				H.exec();
 			});
 		}
-	}
-	H.boot();
-})(wx_helper, jQuery);
+	};
 
+	H.boot = function() {
+		H.addBox().done(function() {
+			H.bindBox().done(function() {
+				H.ready = true;
+			});
+		});
+	};
+
+	$(function() {
+		H.boot()
+	});
+})(wx_helper, jQuery);
